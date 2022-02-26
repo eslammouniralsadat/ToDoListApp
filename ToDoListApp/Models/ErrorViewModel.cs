6f4bf44a718b34f0305c902f92151000372c0516
Eslam Mounir AL Sadat <alsadateslammounir@hotmail.com>
2022-02-26 23:26:40 +0200

    remove default files

--- ToDoListApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ToDoListApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
