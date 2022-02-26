2a07d1eda581a84f3a0f560b69bd96fc27b005f1
Eslam Mounir AL Sadat <alsadateslammounir@hotmail.com>
2022-02-26 23:34:40 +0200

    add model (DoToList)

---
+++ ToDoListApp/Models/ToDoList.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApp.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Content { get; set; }
+
+        [Required]
+        [MaxLength(2500)]
+        public string Note { get; set; }
+    }
+}
