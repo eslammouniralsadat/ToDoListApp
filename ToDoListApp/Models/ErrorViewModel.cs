3bcf95837f758be4416e2ee10323276894f4b8f5
Eslam Mounir AL Sadat <alsadateslammounir@hotmail.com>
2022-02-26 22:26:03 +0200

    Add project files.

---
+++ ToDoListApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToDoListApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
